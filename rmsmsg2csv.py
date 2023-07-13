7ae7b0b4fb8d0923a674157818c9c9c457b8d824
Randy <tarantir@tarantino.us>
2023-07-12 19:41:27 -0500

    Cleaned up most lint highlights

--- rmsmsg2csv.py
+++ rmsmsg2csv.py
@@ -7,14 +7,14 @@
 #
 # Requires:
 #   pip install pandas
-# 
+#
 # Steps have been tested on Windows 10 and Linux Mint 21 +
 #
 # WARNING!! Use at your own risk; it works fine for me!
-# 
-# Author: Randall Tarantino (rtaranti AT gmail DOT com) is a HAM Radio enthusiast 
+#
+# Author: Randall Tarantino (rtaranti AT gmail DOT com) is a HAM Radio enthusiast
 # not affiliated with Winlink Global Radio Email or the Amateur Radio Safety Foundation, Inc.
-# 
+#
 # This code is licensed under:
 #
 # MIT License
@@ -40,9 +40,9 @@
 import os
 import email
 import csv
-import pandas as pd
 import argparse
 from datetime import datetime
+import pandas as pd
 
 P_MSG_PATH = "./Messages/" #specify the path to your Winlink Messages Folder Typically "C:\RMS Express\NOCALL\Messages" where NOCALL = your callsign (trailing backslash required)
 P_DATA_PATH = "./Data/" #specify the path to your Winlink Messages Folder Typically "C:\RMS Express\NOCALL\Data" where NOCALL = your callsign (trailing backslash required)
@@ -53,7 +53,8 @@ F_REGISTRY_WORK_FILE = "Registry.wrk"
 
 parser = argparse.ArgumentParser(description="RMS Message to CSV Utility",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("rms_folder_name", help="Secify RMS Folder e.g. InBox or \"Sent Items\" (if spaces use quotes)")
+parser.add_argument("rms_folder_name",
+                    help="Specify RMS Folder e.g. InBox or \"Sent Items\" (if spaces use quotes)")
 parser.add_argument("csv_filename", help="Specify CSV Output File e.g. ARES.csv")
 args = parser.parse_args()
 
@@ -62,8 +63,8 @@ F_OUTPUT_FILE_PATH = args.csv_filename
 
 def main():
     #Parse Registry.txt to find message ID's in Inbox
-    # (1) Copy Registry.txt to Resistry.wrk as tab (\t) delimited text
-    with open(P_DATA_PATH + F_REGISTRY_FILE ,'r') as f_src, open(P_DATA_PATH + F_REGISTRY_WORK_FILE,'w') as f_wrk:
+    # (1) Copy Registry.txt to Registry.wrk as tab (\t) delimited text
+    with open(P_DATA_PATH + F_REGISTRY_FILE, "r", encoding="utf-8") as f_src, open(P_DATA_PATH + F_REGISTRY_WORK_FILE,"w", encoding="utf-8") as f_wrk:
         f_wrk_header = 'a\tb\tc\td\te\tf\tg\th\ti\tj\tk\tl\tm\tn\to\tp\tq\tr\ts\tt\tu\tv\n'
         f_wrk.write(f_wrk_header)
         for f_wrk_item in f_src:
@@ -71,21 +72,21 @@ def main():
             f_wrk_item = f_wrk_item.replace('\x01', '\t')
             f_wrk.write(f_wrk_item)
     # (3) Use pandas to get RMS Express message-id's for the specified folder e.g. InBox and update workfile
-    df = pd.read_table(P_DATA_PATH + F_REGISTRY_WORK_FILE, sep='\t')
-    df = df.query('i == "' + RMS_FOLDER + '"')
-    df['a'].to_csv(P_DATA_PATH + F_REGISTRY_WORK_FILE, sep='\t', index_label=None, header=False, index=False)
-    with open(F_OUTPUT_FILE_PATH, 'w', newline='') as f_out:
-        fileCounter = 0
+    d_frame = pd.read_table(P_DATA_PATH + F_REGISTRY_WORK_FILE, sep='\t')
+    d_frame = d_frame.query('i == "' + RMS_FOLDER + '"')
+    d_frame['a'].to_csv(P_DATA_PATH + F_REGISTRY_WORK_FILE, sep='\t', index_label=None, header=False, index=False)
+    with open(F_OUTPUT_FILE_PATH, "w", newline="", encoding="utf-8") as f_out:
+        file_counter = 0
         f_out_file = csv.writer(f_out)
         f_out_header = 'rms-date', 'rms-from', 'rms-subject', 'rms-to', 'rms-message-id', 'rms-source', 'rms-sender-location', 'rms-message-body'
         f_out_file.writerow(f_out_header)
         # (4) Parse message files for the selected message-id's only
-        with open(P_DATA_PATH + F_REGISTRY_WORK_FILE) as f_wrk:
+        with open(P_DATA_PATH + F_REGISTRY_WORK_FILE, "r", encoding="utf-8") as f_wrk:
             f_msg_files = f_wrk.read().splitlines() #remove EOL
         for f_msg_file in f_msg_files:
-            isFile = os.path.isfile(P_MSG_PATH + f_msg_file + ".mime")
-            if isFile:
-                f_msg_mime = email.message_from_file(open(P_MSG_PATH + f_msg_file + ".mime", "r"))
+            is_file = os.path.isfile(P_MSG_PATH + f_msg_file + ".mime")
+            if is_file:
+                f_msg_mime = email.message_from_file(open(P_MSG_PATH + f_msg_file + ".mime", "r", encoding="utf-8"))
                 if f_msg_mime.is_multipart():
                     for f_msg_payload in f_msg_mime.get_payload():
                         f_msg_body = f_msg_payload.get_payload()
@@ -94,9 +95,9 @@ def main():
                 # (5) Write summary to CSV e.g. output.csv
                 f_out_item = datetime.strptime(f_msg_mime.get('Date'), '%a, %d %b %Y %H:%M:%S %z'), f_msg_mime.get('From'), f_msg_mime.get('Subject'), f_msg_mime.get('To'), f_msg_mime.get('Message-ID'), f_msg_mime.get('X-Source'), f_msg_mime.get('X-Location'), f_msg_body
                 f_out_file.writerow(f_out_item)
-                fileCounter = fileCounter + 1
+                file_counter = file_counter + 1
     os.remove(P_DATA_PATH + F_REGISTRY_WORK_FILE)
-    print(fileCounter, "Messages Processed!")
+    print(file_counter, "Messages Processed!")
 
 if __name__ == "__main__":
     main()
